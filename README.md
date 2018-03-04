ac535e0b86374f36ebb20b5473778cd4a23cfd99
kko <>
2018-03-04 20:23:39 +0300

    refactoring to meet requirements

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This script searches file _text.txt_ in current working directory and find most used words in it
 
-You can specify input file with parameter -f or --file
+You can specify input file with parameter __-f__ or __--file__
 
 # Quickstart
 
@@ -12,17 +12,18 @@ Example of script launch on Linux, Python 3.5:
 
 ```bash
 
-$ python3 lang_frequency.py 
-и
-в
-не
-что
-он
-на
-с
-как
-его
-я
+$ python3 lang_frequency.py -f text.txt 
+most frequent words:
+word 'и' - 10609 times
+word 'в' - 5307 times
+word 'не' - 4392 times
+word 'что' - 3888 times
+word 'он' - 3813 times
+word 'на' - 3382 times
+word 'с' - 3101 times
+word 'как' - 2133 times
+word 'его' - 1928 times
+word 'я' - 1911 times
 
 
 ```
--- lang_frequency.py
+++ lang_frequency.py
@@ -13,33 +13,36 @@ def load_data(filepath):
         return file_content
 
 
-def get_most_frequent_words(text):
-    punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~–"""
-    replace_punctuation = str.maketrans(' ', ' ', punctuation)
+def get_most_frequent_words(text, word_count):
+    # somehow string.punctuation don't include &ndash;
+    punctuation = string.punctuation + "–"
+    replace_punctuation = str.maketrans(" ", " ", punctuation)
     text = text.lower().translate(replace_punctuation)
     counts = {}
     words = text.split()
     counter = collections.Counter(words)
-    return counter.most_common()[:10]
+    return dict(counter.most_common()[:word_count])
 
 
-def get_file_path():
+def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", dest="file_path",
-                        help="custom text file path")
-    args = parser.parse_args()
-    file_path = args.file_path
-    if file_path is None:
-        file_path = "text.txt"
-    return file_path
+    parser.add_argument(
+        "-f",
+        "--file",
+        dest="file_path",
+        help="custom text file path",
+        default="text.txt"
+    )
+    return parser.parse_args()
 
 
-if __name__ == '__main__':
-    file_path = get_file_path()
+if __name__ == "__main__":
+    file_path = get_args().file_path
     if not os.path.isfile(file_path):
-        print("error: can't find file {}".format(file_path))
-        sys.exit()
+        sys.exit("error: can't find file {}".format(file_path))
     text = load_data(file_path)
-    most_frequent = get_most_frequent_words(text)
+    word_count = 10
+    most_frequent = get_most_frequent_words(text, word_count)
+    print("most frequent words:")
     for word in most_frequent:
-        print(word[0])
+        print("word '{}' - {} times".format(word, most_frequent[word]))
